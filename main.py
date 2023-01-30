92d4e05a399b11d9e5629885665d8df0bcaf6011
AlexeiShigaev <92120105688@inbox.ru>
2023-01-30 10:22:35 +0300

    fixed exceptions in main.py

--- main.py
+++ main.py
@@ -7,16 +7,16 @@ from logs import logger
 
 if __name__ == '__main__':
     logger.info('Запуск бота')
-    loader.bot.polling(none_stop=True, interval=0)
-    # while True:
-    #     try:
-    #         loader.bot.polling(none_stop=True, interval=0)
-    #     except requests.exceptions.ReadTimeout as ex:
-    #         tb = sys.exc_info()[2]
-    #         logger.error('\nПроизошло исключение ReadTimeout\n{}'.format(str(ex.with_traceback(tb))))
-    #     except Exception as ex:
-    #         tb = sys.exc_info()[2]
-    #         logger.error('\nПроизошло исключение\n{}'.format(str(ex.with_traceback(tb))))
-    #     finally:
-    #         sleep(5)
-    #         logger.info('Попытка перезапуска после исключения')
+    # loader.bot.polling(none_stop=True, interval=0)
+    while True:
+        try:
+            loader.bot.polling(none_stop=True, interval=0)
+        except requests.exceptions.ReadTimeout as ex:
+            tb = sys.exc_info()[2]
+            logger.error('\nПроизошло исключение ReadTimeout\n{}'.format(str(ex.with_traceback(tb))))
+        except Exception as ex:
+            tb = sys.exc_info()[2]
+            logger.error('\nПроизошло исключение\n{}'.format(str(ex.with_traceback(tb))))
+        finally:
+            sleep(5)
+            logger.info('Попытка перезапуска после исключения')
